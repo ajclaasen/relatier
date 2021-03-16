41358b08aad65ee5cb6db1178e5fc67b2b28c8de
Raphaël Claasen <raphael.claasen@gmail.com>
2021-03-16 18:08:53 +0100

    Replace existing graph with Cytoscape component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Graph from './Graph';
+import CytoGraph from './CytoGraph';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Graph />
+    <CytoGraph />
   </React.StrictMode>,
   document.getElementById('root')
 );
